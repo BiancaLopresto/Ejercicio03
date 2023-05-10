7b0172c6fed7e4d56753315bec1c672cdbdada7d
BiancaLopresto <bianlopre19@gmail.com>
2023-05-10 15:30:02 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio03.Consola/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+int Grado;
+double Multiplicacion;
+
+Console.Write("Ingrese los grados:");
+Grado = int.Parse(Console.ReadLine());
+
+Multiplicacion = Grado * 0.0174532925;
+
+Console.WriteLine($"Los grados pasados a Radianes son: {Multiplicacion} ");
